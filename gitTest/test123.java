8e186f198761b694708c120ac50a7b1c61a5f2fb
weqas1994 <weqas1994@gmail.com>
2019-06-14 13:48:41 -0400

    method two

--- gitTest/test123.java
+++ gitTest/test123.java
@@ -5,6 +5,11 @@ public class test123{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testgit");
+		test123 obj = new test123();
+		
+	}
+	public void test() {
+		System.out.println("method two");
 	}
 
 }
